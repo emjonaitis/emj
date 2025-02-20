ce32cc2f5e4a271663bf60688d62fb07b13203a8
Erin Jonaitis <jonaitis@dom.wisc.edu>
2025-02-20 10:19:14 -0600

    Created emj_min and emj_max functions to handle NA values

---
+++ R/emj_min.R
@@ -0,0 +1,16 @@
+#' Min and max with NA
+#'
+#' These functions take in a vector of numbers and returns the minimum or maximum.
+#' If no non-missing values are present, NA is returned.
+#' 
+#' @param x Vector to compute minimum or maximum of.
+#' @export
+#' 
+emj_min <- function(x) {
+  x_na <- na.omit(x)
+  if(length(x_na)==0) {
+    NA
+  } else {
+    min(x_na)
+  }
+}
